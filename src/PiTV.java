37e8357e63b122e0d0aa599653171635f9224c59
Ori Korngut <ori.korngut@gmail.com>
2024-01-23 13:02:13 +0200

    Switch with Space only

--- src/PiTV.java
+++ src/PiTV.java
@@ -14,33 +14,33 @@ public class PiTV extends JFrame implements ActionListener, KeyListener {
     public PiTV() {
         // Set up the JFrame
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setUndecorated(true);  // Remove window decorations
-        setExtendedState(JFrame.MAXIMIZED_BOTH);  // Set to full-screen
+        setUndecorated(true);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
         setLayout(new BorderLayout());
 
-        // Load GIFs using file paths
         gifs = new ImageIcon[2];
         gifs[0] = new ImageIcon("C:\\Users\\Excalibur\\IdeaProjects\\PiTV\\src\\GIFs\\robot.gif");
         gifs[1] = new ImageIcon("C:\\Users\\Excalibur\\IdeaProjects\\PiTV\\src\\GIFs\\swerve.gif");
 
         // Create and add the JLabel
         gifLabel = new JLabel(gifs[currentGifIndex]);
+        gifLabel.setSize(getSize());
         add(gifLabel, BorderLayout.CENTER);
 
-        // Add KeyListener to the JFrame
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
 
-        // Show the JFrame
         setVisible(true);
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        // Change the GIF when any key is pressed
-        currentGifIndex = (currentGifIndex + 1) % gifs.length;
-        gifLabel.setIcon(gifs[currentGifIndex]);
+        // Change the GIF when the spacebar is pressed
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            currentGifIndex = (currentGifIndex + 1) % gifs.length;
+            gifLabel.setIcon(gifs[currentGifIndex]);
+        }
     }
 
     // Unused KeyListener methods
@@ -59,4 +59,3 @@ public class PiTV extends JFrame implements ActionListener, KeyListener {
 
     }
 }
-
