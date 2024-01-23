b324f9c5bde6b24031dc8fb46f1efc2283f1c963
Ori Korngut <ori.korngut@gmail.com>
2024-01-23 15:17:54 +0200

    Add all of the gifs

---
+++ src/GIFsConstant.java
@@ -0,0 +1,6 @@
+public enum GIFsConstant {
+    GIFsConstant() {
+        public final String gif = "C:\\Users\\Excalibur\\IdeaProjects\\PiTV\\src\\GIFs\\robot.gif";
+    }
+
+}
--- src/PiTV.java
+++ src/PiTV.java
@@ -1,11 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class PiTV extends JFrame implements ActionListener, KeyListener {
+public class PiTV extends JFrame implements KeyListener {
 
     private JLabel gifLabel;
     private ImageIcon[] gifs;
@@ -14,32 +12,80 @@ public class PiTV extends JFrame implements ActionListener, KeyListener {
     public PiTV() {
         // Set up the JFrame
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setUndecorated(true);
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setUndecorated(true);  // Remove window decorations
+        setExtendedState(JFrame.MAXIMIZED_BOTH);  // Set to full-screen
         setLayout(new BorderLayout());
 
-        gifs = new ImageIcon[2];
+        // Load GIFs using file paths
+        gifs = new ImageIcon[5];
         gifs[0] = new ImageIcon("C:\\Users\\Excalibur\\IdeaProjects\\PiTV\\src\\GIFs\\robot.gif");
         gifs[1] = new ImageIcon("C:\\Users\\Excalibur\\IdeaProjects\\PiTV\\src\\GIFs\\swerve.gif");
+        gifs[2] = new ImageIcon("C:\\Users\\Excalibur\\IdeaProjects\\PiTV\\src\\GIFs\\shot.gif");
+        gifs[3] = new ImageIcon("C:\\Users\\Excalibur\\IdeaProjects\\PiTV\\src\\GIFs\\arm.gif");
+        gifs[4] = new ImageIcon("C:\\Users\\Excalibur\\IdeaProjects\\PiTV\\src\\GIFs\\sensors.gif");
+
 
         // Create and add the JLabel
         gifLabel = new JLabel(gifs[currentGifIndex]);
-        gifLabel.setSize(getSize());
+        gifLabel.setSize(getSize()); // Set the size of the JLabel to the size of the screen
         add(gifLabel, BorderLayout.CENTER);
 
+        // Add KeyListener to the JFrame
         addKeyListener(this);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
 
+        // Show the JFrame
         setVisible(true);
+    }
+
+    private void changeToFirstGif() {
+        // Change to the first GIF when the 'I' key is pressed
+        currentGifIndex = 0;
+        gifLabel.setIcon(gifs[currentGifIndex]);
+    }
+
+    private void changeToSecondGif() {
+        // Change to the second GIF when the 'U' key is pressed
+        currentGifIndex = 1;
+        gifLabel.setIcon(gifs[currentGifIndex]);
+    }
+
+    private void changeToThirdGif() {
+        // Change to the third GIF when the 'Y' key is pressed
+        currentGifIndex = 2;
+        gifLabel.setIcon(gifs[currentGifIndex]);
+    }
+    private void changeToForthGif() {
+        // Change to the third GIF when the 'Y' key is pressed
+        currentGifIndex = 3;
+        gifLabel.setIcon(gifs[currentGifIndex]);
+    }
+    private void changeToFifthGif() {
+        // Change to the third GIF when the 'Y' key is pressed
+        currentGifIndex = 4;
+        gifLabel.setIcon(gifs[currentGifIndex]);
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        // Change the GIF when the spacebar is pressed
-        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-            currentGifIndex = (currentGifIndex + 1) % gifs.length;
-            gifLabel.setIcon(gifs[currentGifIndex]);
+        // Call the corresponding method based on the pressed key
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_I:
+                changeToFirstGif();
+                break;
+            case KeyEvent.VK_U:
+                changeToSecondGif();
+                break;
+            case KeyEvent.VK_Y:
+                changeToThirdGif();
+                break;
+            case KeyEvent.VK_T:
+                changeToFifthGif();
+                break;
+            case KeyEvent.VK_R:
+                changeToForthGif();
+                break;
         }
     }
 
@@ -52,10 +98,5 @@ public class PiTV extends JFrame implements ActionListener, KeyListener {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new PiTV());
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
     }
 }
