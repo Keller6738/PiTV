59a5fb83577588a5a624460d3b5934fd90022183
keller6738 <itaykeller12345@gmail.com>
2024-01-29 11:04:06 +0200

    the code is working now, but the background isn't changing

--- src/PiTV.java
+++ src/PiTV.java
@@ -1,8 +1,14 @@
 import Utils.Constants.RobotGIFs.Gifs;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+
+import static java.awt.BorderLayout.CENTER;
+import static java.awt.Color.WHITE;
+import static java.awt.Frame.MAXIMIZED_BOTH;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class PiTV extends JFrame implements KeyListener {
 
@@ -11,81 +17,76 @@ public class PiTV extends JFrame implements KeyListener {
 
     public PiTV() {
         // Set up the JFrame
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setUndecorated(true);  // Remove window decorations
-        setExtendedState(JFrame.MAXIMIZED_BOTH);  // Set to full-screen
-        setLayout(new BorderLayout());
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setUndecorated(true);  // Remove window decorations
+        this.setExtendedState(MAXIMIZED_BOTH);  // Set to full-screen
+        this.setBackground(WHITE);
+        this.setLayout(new BorderLayout());
 
         // Create and add the JLabel
-        gifLabel = new JLabel(currentIcon);
-        gifLabel.setSize(getSize()); // Set the size of the JLabel to the size of the screen
-        add(gifLabel, BorderLayout.CENTER);
+        this.gifLabel = new JLabel(this.currentIcon);
+        this.gifLabel.setSize(new Dimension(this.currentIcon.getIconWidth(), this.currentIcon.getIconHeight())); // Set the size of the JLabel to the size of the GIF
+        this.gifLabel.setOpaque(true);
+        this.setForeground(WHITE);
+        this.add(this.gifLabel, CENTER);
 
         // Add KeyListener to the JFrame
-        addKeyListener(this);
-        setFocusable(true);
-        setFocusTraversalKeysEnabled(false);
+        this.addKeyListener(this);
+        this.setFocusable(true);
+        this.setFocusTraversalKeysEnabled(false);
 
         // Show the JFrame
-        setVisible(true);
+        this.setVisible(true);
     }
 
     private void changeToFirstGif() {
         // Change to the robot GIF when the 'I' key is pressed
-        currentIcon = Gifs.ROBOT_GIF.getCurrentIcon();
-        gifLabel.setIcon(currentIcon);
+        this.currentIcon = Gifs.ROBOT_GIF.getCurrentIcon();
+        this.gifLabel.setIcon(currentIcon);
     }
 
     private void changeToSecondGif() {
         // Change to the second GIF when the 'U' key is pressed
-        currentIcon = Gifs.SWERVE_GIF.getCurrentIcon();
-        gifLabel.setIcon(currentIcon);
+        this.currentIcon = Gifs.SWERVE_GIF.getCurrentIcon();
+        this.gifLabel.setIcon(currentIcon);
     }
 
     private void changeToThirdGif() {
         // Change to the third GIF when the 'Y' key is pressed
-        currentIcon = Gifs.SHOT_GIF.getCurrentIcon();
-        gifLabel.setIcon(currentIcon);
+        this.currentIcon = Gifs.SHOT_GIF.getCurrentIcon();
+        this.gifLabel.setIcon(currentIcon);
     }
 
     private void changeToForthGif() {
         // Change to the third GIF when the 'T' key is pressed
-        currentIcon = Gifs.ARM_GIF.getCurrentIcon();
-        gifLabel.setIcon(currentIcon);
+        this.currentIcon = Gifs.ARM_GIF.getCurrentIcon();
+        this.gifLabel.setIcon(currentIcon);
     }
 
     private void changeToFifthGif() {
         // Change to the third GIF when the 'R' key is pressed
-        currentIcon = Gifs.SENSORS_GIF.getCurrentIcon();
-        gifLabel.setIcon(currentIcon);
+        this.currentIcon = Gifs.SENSORS_GIF.getCurrentIcon();
+        this.gifLabel.setIcon(currentIcon);
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         // Call the corresponding method based on the pressed key
         switch (e.getKeyCode()) {
-            case KeyEvent.VK_I:
-                changeToFirstGif();
-                break;
-            case KeyEvent.VK_U:
-                changeToSecondGif();
-                break;
-            case KeyEvent.VK_Y:
-                changeToThirdGif();
-                break;
-            case KeyEvent.VK_T:
-                changeToFifthGif();
-                break;
-            case KeyEvent.VK_R:
-                changeToForthGif();
-                break;
+            case KeyEvent.VK_I -> this.changeToFirstGif();
+            case KeyEvent.VK_U -> this.changeToSecondGif();
+            case KeyEvent.VK_Y -> this.changeToThirdGif();
+            case KeyEvent.VK_T -> this.changeToFifthGif();
+            case KeyEvent.VK_R -> this.changeToForthGif();
         }
     }
 
     // Unused KeyListener methods
     @Override
-    public void keyTyped(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {
+    }
 
     @Override
-    public void keyReleased(KeyEvent e) {}
+    public void keyReleased(KeyEvent e) {
+    }
 }
--- src/Utils/Constants.java
+++ src/Utils/Constants.java
@@ -5,11 +5,11 @@ import javax.swing.*;
 public final class Constants {
     public static final class RobotGIFs {
         public enum Gifs {
-            ROBOT_GIF(new ImageIcon("C:\\Users\\Excalibur1\\IdeaProjects\\PiTV\\src\\GIFs\\robot.gif")),
-            SWERVE_GIF(new ImageIcon("C:\\Users\\Excalibur1\\IdeaProjects\\PiTV\\src\\GIFs\\swerve.gif")),
-            SHOT_GIF(new ImageIcon("C:\\Users\\Excalibur1\\IdeaProjects\\PiTV\\src\\GIFs\\shot.gif")),
-            ARM_GIF(new ImageIcon("C:\\Users\\Excalibur1\\IdeaProjects\\PiTV\\src\\GIFs\\arm.gif")),
-            SENSORS_GIF(new ImageIcon("C:\\Users\\Excalibur1\\IdeaProjects\\PiTV\\src\\GIFs\\sensors.gif"));
+            ROBOT_GIF(new ImageIcon("C:\\Users\\PC -23\\IdeaProjects\\PiTV\\src\\GIFs\\robot.gif")),
+            SWERVE_GIF(new ImageIcon("C:\\Users\\PC -23\\IdeaProjects\\PiTV\\src\\GIFs\\swerve.gif")),
+            SHOT_GIF(new ImageIcon("C:\\Users\\PC -23\\IdeaProjects\\PiTV\\src\\GIFs\\shot.gif")),
+            ARM_GIF(new ImageIcon("C:\\Users\\PC -23\\IdeaProjects\\PiTV\\src\\GIFs\\arm.gif")),
+            SENSORS_GIF(new ImageIcon("C:\\Users\\PC -23\\IdeaProjects\\PiTV\\src\\GIFs\\sensors.gif"));
 
             private final ImageIcon currentIcon;
 
