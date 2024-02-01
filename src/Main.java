31a099f1cb33a718fa3d6b8f06db6342a5267c2e
I-No-oNe <145749961+I-No-oNe@users.noreply.github.com>
2024-02-02 00:14:25 +0200

    Added: - DEV mode - AUTO SWITCH mode

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 import javax.swing.*;
-
+/**
+ * press I to change gif
+ * press U to change gif
+ * press Y to change gif
+ * press T to change gif
+ * press R to change gif
+ * PRESS D TO BE ON THE DEV MODE
+ * PRESS A TO BE ON THE AUTO SWITCH MODE
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Code has been started!");
-        // Creating an instance without assigning to a variable
+        System.out.println("The code has been started!");
+        System.out.println("Made by Noam, Ori, and Itay from Excaliber #6738.");
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("The code has been stopped.");
+            System.out.println("Made by Noam, Ori, and Itay from Excaliber #6738.");
+        }));
         SwingUtilities.invokeLater(PiTV::new);
-        System.out.println("Made by Noam,Ori and Itay from Excaliber #6738");
     }
 }
--- src/PiTV.java
+++ src/PiTV.java
@@ -3,73 +3,138 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Objects;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class PiTV extends JFrame implements KeyListener {
     private final JLabel gifLabel;
     private ImageIcon currentIcon;
+    private boolean devMode = false;
+    private boolean autoSwitchEnabled = false;
+    private static final int AUTO_SWITCH_INTERVAL = 10000; // 10 seconds in milliseconds
 
     public PiTV() {
-        // Set up the JFrame
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setUndecorated(true);  // Remove window decorations
-        this.setExtendedState(MAXIMIZED_BOTH);  // Set to full-screen
-        this.getContentPane().setBackground(Color.WHITE); // Set background color of the JFrame
+        this.setUndecorated(true);
+        this.setExtendedState(MAXIMIZED_BOTH);
+        this.getContentPane().setBackground(Color.WHITE);
         this.setLayout(new BorderLayout());
 
-        // Create a JPanel with FlowLayout to center the JLabel
         JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        panel.setOpaque(false); // Make the JPanel transparent
+        panel.setOpaque(false);
         this.add(panel, BorderLayout.CENTER);
 
-        // Create and add the JLabel
         this.gifLabel = new JLabel();
         panel.add(gifLabel);
 
-        // Load the initial GIF
         this.changeToFirstGif();
 
-        // Add KeyListener to the JFrame
         this.addKeyListener(this);
         this.setFocusable(true);
         this.setFocusTraversalKeysEnabled(false);
 
-        // Show the JFrame
         this.setVisible(true);
     }
 
     private void changeToFirstGif() {
-        // Change to the robot GIF when the 'I' key is pressed
         this.currentIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("GIFs/robot.gif")));
         this.gifLabel.setIcon(currentIcon);
+        logGifChange();
     }
 
     private void changeToSecondGif() {
-        // Change to the second GIF when the 'U' key is pressed
         this.currentIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("GIFs/swerve.gif")));
         this.gifLabel.setIcon(currentIcon);
+        logGifChange();
     }
 
     private void changeToThirdGif() {
-        // Change to the third GIF when the 'Y' key is pressed
         this.currentIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("GIFs/shot.gif")));
         this.gifLabel.setIcon(currentIcon);
+        logGifChange();
     }
 
     private void changeToForthGif() {
-        // Change to the third GIF when the 'T' key is pressed
         this.currentIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("GIFs/arm.gif")));
         this.gifLabel.setIcon(currentIcon);
+        logGifChange();
     }
 
     private void changeToFifthGif() {
-        // Change to the third GIF when the 'R' key is pressed
         this.currentIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("GIFs/sensors.gif")));
         this.gifLabel.setIcon(currentIcon);
+        logGifChange();
+    }
+
+    private void logGifChange() {
+        if (devMode) {
+            String filename = currentIcon.getDescription();
+            filename = filename.substring(filename.lastIndexOf('/') + 1); // Extract filename from URL
+            System.out.println("GIF changed: " + filename);
+        }
+    }
+
+    private void toggleDevMode() {
+        devMode = !devMode;
+        if (devMode) {
+            System.out.println("Developer mode is ON.");
+        } else {
+            System.out.println("Developer mode is OFF.");
+        }
+    }
+
+    private void toggleAutoSwitch() {
+        autoSwitchEnabled = !autoSwitchEnabled;
+        if (autoSwitchEnabled) {
+            startAutoSwitchTimer();
+            System.out.println("Auto-switching mode is ON.");
+        } else {
+            stopAutoSwitchTimer();
+            System.out.println("Auto-switching mode is OFF.");
+        }
+    }
+
+    private void startAutoSwitchTimer() {
+        Timer autoSwitchTimer = new Timer();
+        autoSwitchTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (autoSwitchEnabled) {
+                    switchRandomGif();
+                }
+            }
+        }, 0, AUTO_SWITCH_INTERVAL);
+    }
+
+    private void stopAutoSwitchTimer() {
+        // Implement if needed
+    }
+
+    private void switchRandomGif() {
+        Random random = new Random();
+        int randomIndex = random.nextInt(5); // Generate a random index between 0 and 4
+        switch (randomIndex) {
+            case 0:
+                changeToFirstGif();
+                break;
+            case 1:
+                changeToSecondGif();
+                break;
+            case 2:
+                changeToThirdGif();
+                break;
+            case 3:
+                changeToForthGif();
+                break;
+            case 4:
+                changeToFifthGif();
+                break;
+        }
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        // Call the corresponding method based on the pressed key
         switch (e.getKeyCode()) {
             case KeyEvent.VK_I:
                 this.changeToFirstGif();
@@ -86,17 +151,21 @@ public class PiTV extends JFrame implements KeyListener {
             case KeyEvent.VK_R:
                 this.changeToForthGif();
                 break;
+            case KeyEvent.VK_D:
+                toggleDevMode();
+                break;
+            case KeyEvent.VK_A:
+                toggleAutoSwitch();
+                break;
         }
     }
 
-    // Unused KeyListener methods
     @Override
-    public void keyTyped(KeyEvent e) {}
-
-    @Override
-    public void keyReleased(KeyEvent e) {}
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(PiTV::new);
+    public void keyTyped(KeyEvent e) {
     }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
 }
