4ec3f1b00ead13de78738d2529380fd9b2a4a734
I-No-oNe <145749961+I-No-oNe@users.noreply.github.com>
2024-05-01 22:04:42 +0300

    Sort a little thing on Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         /*TODO:
         1.Finish the Gui
-        2.Fix images(make them again)
+        2.Fix images(make them again/re-do them again)
         3.Change Images names
         */
         System.out.println("The code has been started!");
         System.out.println("Made by Itay and Noam from Excalibur #6738.");
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             System.out.println("The code has been stopped.");
-            System.out.println("Made by Itay Keller and Noam Novoselski from Excalibur #6738.");
+            System.out.println("Made by Itay Keller and Noam Novoselsky from Excalibur #6738.");
         }));
         new Screen();
     }
